c04d54ff049861e14116979445342bea93c3eb99
Carsten Bönsel <CBoensel@users.noreply.github.com>
2023-08-31 10:40:01 +0200

    Update paypal.js

--- src/lib/paypal.js
+++ src/lib/paypal.js
@@ -21,6 +21,9 @@ async function initPayPalButtons(clientID = 'test', containerID, cartTotal = '0.
         },
         createOrder: function (data, actions) {
           // Set up the transaction
+          // @info This is just for testing. 
+          // Refer to your server-side integration / your PSP endpoing here 
+          // and return the order id
           return actions.order.create({
             purchase_units: [
               {
@@ -33,14 +36,23 @@ async function initPayPalButtons(clientID = 'test', containerID, cartTotal = '0.
         },
         onApprove: function (data, actions) {
           // Capture order after payment approved
+          // @info This is just for testing. 
+          // Refer to your server-side integration / your PSP endpoing here 
           return actions.order.capture().then(function (details) {
             alert("Payment successful!");
           });
         },
         onError: function (err) {
           // Log error if something goes wrong during approval
+          // Handle error messaging for the buyer
           alert("Something went wrong");
           console.log("Something went wrong", err);
+        },
+        onCancel: function () {
+          // Log error if something goes wrong during approval
+          // Handle info messaging for the buyer
+          alert("Payment was cancelled");
+          console.log("Payment was cancelled");
         },
       }
       ).render(containerID);
